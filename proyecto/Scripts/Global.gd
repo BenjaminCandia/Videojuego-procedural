934b344de0fd7635af4978775dace54ab69712ba
GIS ROA <gis.roa@duocuc.cl>
2025-10-23 23:25:57 -0300

    WIP shop changes

--- proyecto/Scripts/Global.gd
+++ proyecto/Scripts/Global.gd
@@ -1,6 +1,24 @@
-extends Node  # No CanvasLayer
+extends Node
 
-var coins_total := 4  # empieza con 4 monedas
+# Variables globales
+var coins_total := 0
+var elapsed_time := 0.0
+var timer_running := false
 
+# Aumenta monedas
 func award_coin():
 	coins_total += 1
+
+# Reinicia y activa cronómetro
+func start_timer():
+	elapsed_time = 0.0
+	timer_running = true
+
+# Detiene cronómetro
+func stop_timer():
+	timer_running = false
+
+# Guarda tiempo de nivel (opcional si quieres histórico)
+var level_times := {}
+func save_level_time(level_name: String, time: float):
+	level_times[level_name] = time
